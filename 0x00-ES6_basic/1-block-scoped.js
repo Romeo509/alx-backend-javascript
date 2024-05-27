dacf889215ed4dd27e47cbb040f82e2708686b42
Romeo509 <romeoameku503@gmail.com>
2024-05-27 21:45:50 +0000

    task1 solved

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     let task2 = true; // Change var to let
   
     if (trueOrFalse) {
-      task = true; // No need to re-declare with let
-      task2 = false; // No need to re-declare with let
+      const task = true; // No need to re-declare with let
+      const task2 = false; // No need to re-declare with let
     }
   
     return [task, task2];
