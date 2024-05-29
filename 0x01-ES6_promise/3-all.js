b451715a3e2dceb138709d2a58dde3a2860f5017
Romeo509 <romeoameku503@gmail.com>
2024-05-29 13:16:35 +0000

    update

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+// 3-all.js
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const { body } = results[0];
+      const { firstName, lastName } = results[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
