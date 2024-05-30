c442a50e63e6bbc619679399acf8088eaefb10ed
Romeo509 <romeoameku503@gmail.com>
2024-05-30 02:17:20 +0000

    feat: Implement asyncUploadUser for concurrent photo and user creation (assignment #Task 10)

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
