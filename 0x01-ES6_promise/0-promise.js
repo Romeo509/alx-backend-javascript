242a1f42c9af1ac58cf0aa0cc9659dc14f2552e5
Romeo509 <romeoameku503@gmail.com>
2024-05-30 04:02:48 +0000

    wide fix

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  // eslint-disable-next-line no-unused-vars
+  return new Promise((resolve, reject) => {
+  // This is a simple Promise that doesn't do anything specific yet
+  });
+}
