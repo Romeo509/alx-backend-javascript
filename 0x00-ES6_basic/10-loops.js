f10f6770cc901434d61829f43aeb010f30211cc8
Romeo509 <romeoameku503@gmail.com>
2024-05-30 04:12:56 +0000

    wide fix

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const myarray = [];
-  for (const item of array) {
-    myarray.push(appendString + item);
-  }
-
-  return myarray;
-}
--- 0x00-ES6_basic/README.md
+++ 0x00-ES6_basic/README.md
@@ -1,1 +1,1 @@
-README
+In progress
