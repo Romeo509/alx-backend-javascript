0f2db2dee95b50fad35dd0c4d859954ca1ca552f
Romeo509 <romeoameku503@gmail.com>
2024-05-31 00:02:36 +0000

    eslint-disable no-underscore-dangle

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,13 +1,7 @@
+/* eslint-disable no-underscore-dangle */
+
 export default class Currency {
   constructor(code, name) {
-    /**
-   * Represents a currency with a code and a name.
-   *
-   * @constructor
-   * @param {string} code - The ISO 4217 currency code (e.g., USD, EUR, JPY).
-   * @param {string} name - The full name of the currency (e.g., United States Dollar, Euro, Japanese Yen).
-   * @throws {Error} - If either code or name is not a string.
-   */
     if (typeof code !== 'string' || typeof name !== 'string') throw new Error();
     this.code = code;
     this.name = name;
