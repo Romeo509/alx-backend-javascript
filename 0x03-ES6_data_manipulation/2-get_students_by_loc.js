6da8c13ea80d909517ae6d7a679a9bc93e2dca29
Romeo509 <romeoameku503@gmail.com>
2024-06-04 16:56:07 +0000

    function: returns an array of  object located in specific city

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+// Function to return an array of object located in specific city
+import getListStudents from './0-get_list_students';
+
+export default function getStudentsByLocation(student = getListStudents(), city) {
+  return student.filter((el) => el.location === city);
+}
