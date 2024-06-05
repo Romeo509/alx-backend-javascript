0f1c44c4289017459b130c918375badf6ee49c4f
Romeo509 <romeoameku503@gmail.com>
2024-06-05 15:14:58 +0000

    task: 9 files completed

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,4 @@
+// returns a new ArrayBuffer with an Int8 value at a specific position.
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) throw Error('Position outside range');
   const buffer = new ArrayBuffer(length);
