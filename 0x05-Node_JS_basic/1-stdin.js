3ddf09ca3aeea9b3bd6f7c4b8eaf209e8999b099
Romeo509 <romeoameku503@gmail.com>
2024-07-24 01:03:35 +0000

    task 1 solved

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,21 @@
+// 1-stdin.js
+
+// Display a welcome message and prompt the user for their name
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+// Listen for 'readable' events on the standard input
+process.stdin.on('readable', () => {
+  // Read a chunk of data from the input stream
+  const chunk = process.stdin.read();
+
+  // If there is data, write it to standard output
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+// Listen for the 'end' event on the standard input
+process.stdin.on('end', () => {
+  // Display a closing message when the input stream ends
+  process.stdout.write('This important software is now closing\n');
+});
