aec9e70306011c21f0752124035658291bd199df
Romeo509 <romeoameku503@gmail.com>
2024-07-24 00:57:44 +0000

    task 1 solved

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,27 +1,15 @@
-// 1-stdin.js
-
-// Write a prompt to the user
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Check if the input is from a terminal or piped
 if (process.stdin.isTTY) {
-  // Handle data input when running interactively
   process.stdin.on('data', (data) => {
-    // Output the user's name
     process.stdout.write(`Your name is: ${data.toString()}`);
-    // Exit the process after handling the input
     process.exit();
   });
 } else {
-  // Handle data input when input is piped in
   process.stdin.on('data', (data) => {
-    // Output the user's name
     process.stdout.write(`Your name is: ${data.toString()}`);
-    // Exit the process after handling the input
     process.exit();
   });
-
-  // Handle process exit to display a closing message
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
   });
