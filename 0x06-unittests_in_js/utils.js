814fd5449585f4e9b18a3d630eebb0a81f0bd311
Romeo509 <romeoameku503@gmail.com>
2024-07-25 20:59:38 +0000

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,22 @@
+// utils.js
+const Utils = {
+    calculateNumber: function(type, a, b) {
+      a = Math.round(a);
+      b = Math.round(b);
+      
+      if (type === 'SUM') {
+        return a + b;
+      } else if (type === 'SUBTRACT') {
+        return a - b;
+      } else if (type === 'DIVIDE') {
+        if (b === 0) {
+          return 'Error';
+        }
+        return a / b;
+      }
+      return null;
+    }
+  };
+  
+  module.exports = Utils;
+  
